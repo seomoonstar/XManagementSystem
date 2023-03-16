531faa1ace462ee998af6dd7a3e354267b411560
samoo <samoo@192.168.0.33>
2023-03-16 15:07:31 +0900

    ;init commit

---
+++ xManangementSystem/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author samoo
+ *
+ */
+module xManangementSystem {
+}
---
+++ xManangementSystem/src/xManangementSystem/Hellow.java
@@ -0,0 +1,10 @@
+package xManangementSystem;
+
+public class Hellow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
