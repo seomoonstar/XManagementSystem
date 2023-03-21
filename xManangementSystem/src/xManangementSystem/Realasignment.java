665a39b3791dbe598781e88e464cf7fafa28402c
samoo <samoo@192.168.0.33>
2023-03-21 14:31:39 +0900

    assignment

---
+++ xManangementSystem/src/xManangementSystem/Realasignment.java
@@ -0,0 +1,16 @@
+package xManangementSystem;
+
+import java.util.Scanner;
+
+public class Realasignment {
+
+	public static void main(String[] arguement) {
+		
+	     System.out.println("1.Add Food Materials");
+	     System.out.println("2.Delete Food Materials");
+	     System.out.println("3.Edit Food Materials");
+	     System.out.println("4.View Food Materials");
+	     System.out.println("5.Exit");
+	     
+	}
+}
