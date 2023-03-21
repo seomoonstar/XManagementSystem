db4d7439a76053c8a9d03fc552266a9d2f8654ed
samoo <samoo@192.168.0.33>
2023-03-21 14:42:50 +0900

    k;jhjhgf

---
+++ xManangementSystem/src/xManangementSystem/Assignment.java
@@ -0,0 +1,107 @@
+package xManangementSystem;
+import java.util.Scanner;
+
+
+public class Assignment {
+	/*
+	public static void main(String[] args) {
+
+	      System.out.printf("Hello World!!");
+	      
+	      System.out.printf("%.2f\n",3.141592);
+	      System.out.printf("%d\n",10);
+	      System.out.printf("%o\n",10);
+	      System.out.printf("%x\n",10);
+	      System.out.printf("%c\n",'a');
+	      System.out.printf("%s\n","abc");
+	      
+	      
+
+	}
+	
+	  public static void main(String[] arguement) {
+	      String stick, sword;
+	      
+	      Scanner scan = new Scanner(System.in);
+	      
+	      stick = scan.nextLine();
+	      sword = scan.next();
+	      
+	      System.out.println(stick);
+	      System.out.println(sword);
+	      
+	   }
+	   
+	   
+	   }
+	   
+	   public static void main(String[] arguement) {
+	      int a;
+	      double b;
+	      
+	      Scanner scan = new Scanner(System.in);
+	      a = scan.nextInt();
+	      b = scan.nextDouble();
+	      
+	      System.out.println("a = " + a);
+	      System.out.println("b = " + b);
+	      
+	      
+	   }
+	   
+	   public static void main(String[] arguement) {
+	      int x;
+	      int y;
+	      int sum, miu, mul, div, res;
+	      
+	      Scanner scan = new Scanner(System.in);
+	      System.out.println("정수 2개를 입력해 주세요: ");
+	      x = scan.nextInt();
+	      y = scan.nextInt();
+	      
+	      sum = x + y;
+	      miu = x - y;
+	      mul = x * y;
+	      div = x / y;
+	      res = x % y;
+	      
+	      System.out.println("합: " + sum);
+	      System.out.println("차: " + miu);
+	      System.out.println("곱셈: " + mul);
+	      System.out.println("나눗셈: "+ div);
+	      System.out.println("나머지: " + res);
+	      
+	   }
+	public static void main(String[] arguement) {
+	     int celsiusValue;
+	      
+	     double fahrenheitValue;
+	      
+	     Scanner scan = new Scanner(System.in);
+	     
+	     System.out.println("섭씨온도를 입력해 주세요: ");
+	      
+	     celsiusValue = scan.nextInt();
+	      
+	     fahrenheitValue = 9.0/5.0 * celsiusValue + 32.0;
+	      
+	     System.out.println(" 섭씨 온도 = "+ celsiusValue);
+	      
+	     System.out.println(" 화씨 온도 = "+fahrenheitValue);
+	      
+	      
+	}*/
+	
+	public static void main(String[] arguement) {
+	      String echo;
+	      
+	      Scanner scan = new Scanner(System.in);
+	      echo = scan.nextLine();
+	      
+	      System.out.println("str = " + echo);
+	}
+
+}
+
+
+
--- xManangementSystem/src/xManangementSystem/Hellow.java
+++ xManangementSystem/src/xManangementSystem/Hellow.java
@@ -1,10 +1,42 @@
 package xManangementSystem;
+import java.util.Scanner;
 
 public class Hellow {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hellow");
+		/*
+		int num;
+		System.out.print("Input Number: ");
+		Scanner scan = new Scanner(System.in);
+		
+		num = scan.nextInt();
+		
+		for(int i = 0 ; i<num ; i++) {
+			int cou = scan.nextInt();
+		
+			if(cou>0) {
+				System.out.println("positive number");
+			}
+			else if(cou == 0) {
+				System.out.println("Zero");
+			}
+			else {
+				System.out.println("negative number");
+			}
+		}*/
+		
+		int num;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.printf("숫자를 입력해 주세용: ");
+		num = scan.nextInt();
+		
+		if((num %2 ) == 0 ) {
+			System.out.println(num + " 은 짝수입니다.");
+		}
+		else {
+			System.out.println(num + " 은 홀수입니다.");
+		}
 	}
-
 }
